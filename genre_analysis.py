ea1fcbdc1cc97a2b54b311d4ab9909f9ebbe5dde
Sakshisak <sakshisa@iitk.ac.in>
2021-11-18 05:26:11 +0530

    Most Watched Genre Analysis

--- genre_analysis.py
+++ genre_analysis.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import mean
 import pandas as pd
 import numpy as np
 import json
@@ -26,7 +27,6 @@ df['genres'] = df_dict_to_list
 genre_list = df['genres'].explode().unique()
 
 
-
 # plot genre vs no of anime
 dict_count = {}
 
@@ -35,7 +35,6 @@ for genre in genre_list:
     dict_count[genre] = len(df_genre.index)
 
 dict_count = dict(sorted(dict_count.items(), key=lambda item: item[1]))
-print(dict_count)
 
 ax = plt.subplot(111)
 plt.figure(figsize=(25,10))
@@ -46,3 +45,27 @@ plt.xticks(range(len(dict_count)), list(dict_count.keys()), rotation=-45, ha='le
 plt.title('No of Anime vs Genre')
 plt.subplots_adjust(bottom=0.2)
 plt.savefig(OUT_DIR+'most_common_genre.jpg')
+plt.clf()
+
+
+# plot genre vs avg-no-users
+dict_watch_count = {}
+
+for genre in genre_list:
+    df_genre = df[[(genre in g_list) for g_list in df['genres']]]
+    mean_no = df_genre['num_list_users'].mean()
+    mean_no = mean_no.sum(axis=0)
+    dict_watch_count[genre] = mean_no
+
+dict_watch_count = dict(sorted(dict_watch_count.items(), key=lambda item: item[1]))
+
+ax = plt.subplot(111)
+plt.figure(figsize=(25,10))
+plt.bar(range(len(dict_watch_count)), list(dict_watch_count.values()), align='center')
+plt.xlabel('Genres')
+plt.ylabel('Average No of Viewers')
+plt.xticks(range(len(dict_watch_count)), list(dict_watch_count.keys()), rotation=-45, ha='left')
+plt.title('No of Viewers vs Genre')
+plt.subplots_adjust(bottom=0.2)
+plt.savefig(OUT_DIR+'most_watched_genre.jpg')
+plt.clf()
