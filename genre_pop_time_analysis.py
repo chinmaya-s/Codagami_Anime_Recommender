7ab43dae25e49d143c79bf9c39f805ceafe48281
Sakshisak <sakshisa@iitk.ac.in>
2021-11-20 04:36:00 +0530

    Take 15 genres for time analysis

--- genre_pop_time_analysis.py
+++ genre_pop_time_analysis.py
@@ -25,22 +25,21 @@ df['start_date'] = df['start_date'].astype(int)
 df = df[df['start_date'] < 2022]
 df['start_date'] = (df['start_date']//5)*5
 
-# get list of genres
-genre_list = df['genres'].explode().unique()
+# get list of genres for grouping and start dates for x_ticks
+genre_list = ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Hentai', 'Historical', 'Kids', 'Music', 'Romance', 'School', 'Sci-Fi', 'Shounen', 'Slice of Life', 'Supernatural']
 x = df['start_date'].tolist()
-ax = plt.gca()
 
 # get the plot 
+ax = plt.gca()
 for genre in genre_list:
     df_genre = df[[(genre in g_list) for g_list in df['genres']]]
     df_genre_group = df_genre.groupby('start_date').agg({'num_list_users': 'mean'}).reset_index()
-    df_genre_group.plot( x='start_date' , y='num_list_users', rot=-45, ax=ax, figsize=(25,10))
+    df_genre_group.plot( x='start_date' , y='num_list_users', rot=-45, ax=ax, figsize=(20,10))
 
-plt.legend(genre_list, bbox_to_anchor=(1,1.1))
-plt.xlabel('Start Year')
-plt.ylabel('Average No of Viewers')
-plt.title('Popularity Time Analysis of Genres')
-plt.subplots_adjust(bottom=0.15, right=0.85)
+plt.legend(genre_list)
+plt.xlabel('Start Year', fontsize=18)
+plt.ylabel('Average No of Viewers', fontsize=18)
+plt.title('Popularity Time Analysis of Genres', fontsize=22)
 fig = plt.gcf()
 fig.set_size_inches(20, 10)
 fig.savefig(OUT_DIR+'genre_popularity_time_analysis.jpg')
