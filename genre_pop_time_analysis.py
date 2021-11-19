d4fa4a3b29001297e54216d697ccdd9093f2b9bc
Sakshisak <sakshisa@iitk.ac.in>
2021-11-20 04:16:28 +0530

    Removed anime with start year 2022

--- genre_pop_time_analysis.py
+++ genre_pop_time_analysis.py
@@ -22,6 +22,7 @@ df['start_date'] = pd.to_datetime(df['start_date'])
 df['start_date'] = df['start_date'].dt.year
 df.dropna(inplace=True)
 df['start_date'] = df['start_date'].astype(int)
+df = df[df['start_date'] < 2022]
 df['start_date'] = (df['start_date']//5)*5
 
 # get list of genres
@@ -36,7 +37,7 @@ for genre in genre_list:
     df_genre_group.plot( x='start_date' , y='num_list_users', rot=-45, ax=ax, figsize=(25,10))
 
 plt.legend(genre_list, bbox_to_anchor=(1,1.1))
-plt.xlabel('Time Period')
+plt.xlabel('Start Year')
 plt.ylabel('Average No of Viewers')
 plt.title('Popularity Time Analysis of Genres')
 plt.subplots_adjust(bottom=0.15, right=0.85)
