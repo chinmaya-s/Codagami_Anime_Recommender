397af8203fadd54b2f86321a412d620560e6419d
Chinmaya Singal <chinmayasingal@gmail.com>
2021-11-19 15:49:04 +0530

    Remove unnecessary data

--- tf_idf_synopsis_recommender.py
+++ tf_idf_synopsis_recommender.py
@@ -9,38 +9,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-path_in_str = 'user_data1.0.json'
-json_file = open(path_in_str)
-data = json.load(json_file)
-df = pd.DataFrame.from_dict(data, orient='index')
-df = df[df['error'].isna()]
-
-
-df['user'] = df.index
-df.reset_index(inplace=True)
-df['user_id'] = df.index
-
-
-def melt_series(s):
-    lengths = s.str.len().values
-    flat = [i for i in itertools.chain.from_iterable(s.values.tolist())]
-    idx = np.repeat(s.index.values, lengths)
-    return pd.Series(flat, idx, name=s.name)
-
-
-user_df = melt_series(df.data).to_frame().join(df.drop('data', 1))
-
-
-user_df['anime_id'] = user_df['data'].apply(lambda x : x['node']['id'])
-user_df['status'] = user_df['data'].apply(lambda x : x['list_status']['status'])
-user_df['score'] = user_df['data'].apply(lambda x : x['list_status']['score'])
-user_df['is_rewatching'] = user_df['data'].apply(lambda x : x['list_status']['is_rewatching'])
-
-
-
-user_df = user_df[['user', 'user_id', 'anime_id', 'status', 'score', 'is_rewatching']]
-user_df = user_df.rename({'score': 'user_score'}, axis=1)
-
 
 path_in_str = 'data/anime_list_final_231.json'
 json_file = open(path_in_str)
@@ -63,17 +31,6 @@ anime_df['on_hold'] = anime_df['statistics'].apply(lambda x : x['status']['on_ho
 anime_df.drop(['genres', 'statistics'], axis=1, inplace=True)
 
 anime_df.rename({'id': 'anime_id'}, axis=1, inplace=True)
-
-df_merge_raw = pd.merge(anime_df, user_df, on = 'anime_id')
-
-df_merge = df_merge_raw[['anime_id', 'title', 'genres_name', 'num_episodes', 'mean', 'num_list_users', 'user_id', 'user_score']]
-
-df_merge.rename({'title': 'name', 'genres_name': 'genre', 'num_episodes': 'episodes', 'mean': 'rating_x', 'num_list_users': 'members', 'user_score': 'rating_y'}, axis=1, inplace= True)
-
-
-df_merge['anime_id'] = df_merge['anime_id'].astype(int)
-df_merge['episodes'] = df_merge['episodes'].astype(int)
-df_merge['type'] = 'TV'
 
 nltk.download('wordnet')
 nltk.download('stopwords')
@@ -150,4 +107,4 @@ def recommend_anime(title, max_reco = 10, cosine_sim = cos_sim):
         recommended_animes.append(anime_name)
     return recommended_animes
 
-recommend_anime('Fate/Zero', max_reco=20)
+print(recommend_anime('Fate/Zero', max_reco=20))
