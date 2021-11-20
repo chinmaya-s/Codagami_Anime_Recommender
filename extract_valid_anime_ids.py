f8937c477be57381d989cde47c7912d1740f8a24
Sakshisak <sakshisa@iitk.ac.in>
2021-11-20 16:47:16 +0530

    Updated Readme script section

---
+++ extract_valid_anime_ids.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "https://myanimelist.net/info.php?search=%25%25%25&go=relationids&divname=relationGen1"
+r = requests.get(URL)
+# print(r.content)
+
+soup = BeautifulSoup(r.content, 'html5lib')
+# print(soup.prettify())
+
+ids=[]
+
+ids_list1 = soup.find_all('td', attrs = { 'class':'td1'}) 
+ids_list2 = soup.find_all('td', attrs = {'class': 'td2'})
+# print(ids_list)
+
+for id in ids_list1:
+    a = id.a
+    if(a):
+        ids.append(a.text)
+
+for id in ids_list2:
+    a = id.a
+    if(a):
+        ids.append(a.text)
+
+print(len(ids))
+sorted(ids)
+
+with open('anime_ids.txt', 'w') as f:
+    for item in ids:
+        f.write("%s\n" % item)
